9c5e4869b62f66872697cbed06a5d0ff5bb79047
trix0 <matus.satara@gmail.com>
2018-02-01 16:18:46 +0100

    update connect

--- connect.js
+++ connect.js
@@ -12,6 +12,8 @@ const cv = require('opencv4nodejs');
 ///////////// images for img recognition////////////////////
 const img2 = cv.imread('./autoTest/CreateAccount.png');
 const img3 = cv.imread('./autoTest/LoginButton.png');
+const imgAllowButton = cv.imread('./autoTest/allowButton.png');
+const imgDenyButton = cv.imread('./autoTest/denyButton.png');
 const imgLoginEmail = cv.imread('./autoTest/loginEmail.png');
 const imgBigOkButton = cv.imread('./autoTest/bigOkButton.png');
 const imgloginPassed = cv.imread('./autoTest/loginPassed.png');
@@ -40,7 +42,7 @@ const opts = {
         appPackage:"com.pointvoucher.playlondonpv",
         appActivity:"com.unity3d.player.UnityPlayerActivity",
         fullReset:false,
-        noReset:true,
+        noReset:false,/////////// false adds around 3Sec
         //noPermsCheck:true,
         //launch:true,
         //autoGrantPermissions:true,
@@ -85,6 +87,7 @@ async function fnInit(){
     performance.measure('A to B', 'A', 'B');
     let measure = performance.getEntriesByName('A to B')[0];
     console.log("Loading Time "+measure.duration/1000+" s"); 
+    await fnPermission(5,true,client);
     const loading=await fnLoading(40,client); // loading time 40 screenshots request afterwards error
     if(loading==1){
       console.log("running test1");
@@ -121,8 +124,8 @@ async function fnConnectStf(serial){
       return api.devices.getDeviceBySerial({
       serial: serial
       , fields: 'serial,present,ready,using,owner'
+      })
     })
-   })
     let device = getDevices.obj.device;
     const avaiDevices= await clientSwag.then((api)=>{
       if (!device.present || !device.ready || device.using || device.owner) {
@@ -164,35 +167,36 @@ async function fnConnectStf(serial){
 
 // disconnect from OPenSTf
 function fnDisconnect(UDID){
-
   clientSwag.then(function(api) {
     return api.user.getUserDevices({
       serial: serial
     , fields: 'serial,present,ready,using,owner'
-    }).then(function(res) {
+    })
+    .then(function(res) {
         // check if device can be added or not
-        var devices = res.obj.devices
+      var devices = res.obj.devices
 
-        var hasDevice = false
-        devices.forEach(function(device) {
-          if(device.serial === serial) {
-            hasDevice = true;
-          }
-        })
-
-        if (!hasDevice) {
-          throw new Error('You are not owner')
+      var hasDevice = false
+      devices.forEach(function(device) {
+        if(device.serial === serial) {
+          hasDevice = true;
         }
+      })
 
-        return api.user.deleteUserDeviceBySerial({
-          serial: serial
-        }).then(function(res) {
-          if (!res.obj.success) {
-            throw new Error('Could not disconnect to device')
+      if (!hasDevice) {
+        throw new Error('You are not owner')
+      }
 
-          }
-          console.log("Disconnected!")
-        })
+      return api.user.deleteUserDeviceBySerial({
+        serial: serial
+      })
+      .then(function(res) {
+        if (!res.obj.success) {
+          throw new Error('Could not disconnect to device')
+
+        }
+        console.log("Disconnected!")
+      })
     })
   })
 
@@ -204,18 +208,18 @@ function fnCreateServer(port){
     let appiumPort=port
     let appiumServer=exec('appium -p '+port+' --session-override --log-timestamp --log /home/trixo/Code/javaRewrite/log.txt');
     appiumServer.stdout.on('data', function(data) {
-        if(data.indexOf("listener started ")!=-1){
+      if(data.indexOf("listener started ")!=-1){
 
-          console.log("Server Created");
-          resolve(true);
-          
-        }
+        console.log("Server Created");
+        resolve(true);
+        
+      }
     });
     appiumServer.stderr.on('data', function(data) {
-        reject(data)
+      reject(data)
     });
     appiumServer.on('close', function(code) {
-        reject(code);
+      reject(code);
     });
 // ...
 
@@ -225,18 +229,15 @@ function fnCreateServer(port){
 
 //rconnect to device 
 function fnConnect(UDID){
-
   return new Promise((resolve,reject)=>{
-
-
-
     const client = wdio.remote(opts);
     client.init(opts, (error) =>{
       if (error) {
         reject(new Error('Session did not start properly. Please make sure you sauce credentials are correct'));
       }    
     
-    }).then(()=>{
+    })
+    .then(()=>{
       return(client);
     })   
     
@@ -250,6 +251,67 @@ function timeout(delay){
     setTimeout(resolve,delay)
   })
 }
+
+
+
+function fnPermission(repeat,bValue,client){
+  if(bValue===null){
+    return true;
+  }
+  let iCounter = 0;
+  const attempt = () => fnPermssionOnce(iCounter,bValue,client).catch(err => {
+      console.log(err.message);
+      iCounter++;
+      if (iCounter === repeat) {
+          // Failed, out of retries
+          return Promise.reject( new Error("Couldnt find permission screen"));
+      }
+      // Retry 
+      return attempt();
+  });
+  return attempt();    
+}
+
+
+function fnPermssionOnce(iCounter,bValue,client){
+  return new Promise(resolve=>{
+    let testId=0;
+    let img;
+    if(bValue===true){
+      img=imgAllowButton;
+    }
+    else if(bValue===false){
+      img=imgDenyButton; 
+    }
+    else{
+      return true;
+    }
+    return client.screenshot()
+      .then((data) => {            
+        let buf = new Buffer(data.value, 'base64');
+        let img1 = cv.imdecode(buf)
+          let result = img1.matchTemplate(img, 5).minMaxLoc(); 
+
+          if (result.maxVal >= 0.65) {
+              // Fail
+              
+          }
+          else{
+            const msg = "WAiting for permission pop up#"+iCounter;
+            throw new Error(msg);
+          }
+          // All good
+          fnClick(img,client,repeats=5,"Click on permission button",0).then(()=>{
+            resolve();
+          });
+          
+        
+
+    });    
+  })
+
+}
+
 
 
 /// main loading function 
@@ -271,33 +333,32 @@ function fnLoading(repeat,client){
 
 // loading once //// used to repeat in fnLoading()
 function fnIsLoadingOnce(iCounter,client){
-let testId=0;
-return client.screenshot()
-        .then((data) => {
+  let testId=0;
+  return client.screenshot()
+    .then((data) => {            
+      let buf = new Buffer(data.value, 'base64');
+      let img1 = cv.imdecode(buf)
+      for(var t=0; t<2; t++){
+        let result = img1.matchTemplate(img2, 5).minMaxLoc(); 
+        let result2 = img1.matchTemplate(img3, 5).minMaxLoc(); 
+
+        if (result.maxVal >= 0.65) {
+            // Fail
+            testId=1;
             
-            let buf = new Buffer(data.value, 'base64');
-            let img1 = cv.imdecode(buf)
-            for(var t=0; t<2; t++){
-              let result = img1.matchTemplate(img2, 5).minMaxLoc(); 
-              let result2 = img1.matchTemplate(img3, 5).minMaxLoc(); 
+        }
+        else if(result2.maxVal>=0.65){
+            testId=2;
+        }
+        else{
+          const msg = "Still Loading #"+iCounter;
+          throw new Error(msg);
+        }
+        // All good
+        return testId;
+      }
 
-              if (result.maxVal >= 0.65) {
-                  // Fail
-                  testId=1;
-                  
-              }
-              else if(result2.maxVal>=0.65){
-                  testId=2;
-              }
-              else{
-                const msg = "Still Loading #"+iCounter;
-                throw new Error(msg);
-              }
-              // All good
-              return testId;
-            }
-
-        });
+  });
 }
 
 
@@ -310,7 +371,7 @@ return client.screenshot()
 /// clears keyboard 
 function fnClearKeyBoard(client){
   return new Promise((resolve,reject)=>{
-   client.touchPerform([{
+    client.touchPerform([{
       action: 'tap',
       options: {
         x: 0,   // x offset
@@ -318,8 +379,8 @@ function fnClearKeyBoard(client){
         count: 1 // number of touches
       }
       }])
-   .then(()=>{
-    resolve(true);
+    .then(()=>{
+      resolve(true);
     }).catch((err)=>{
       reject(err);
     })
@@ -348,50 +409,47 @@ function fnIsOnScreen(img,client, repeats = 5, desc, wait = 2000) {
     
    
 }
-
-
 /////// function used in fnIsOnScreen() to repeat 
 function fnIsOnScreenOnce(img, desc,iCounter,client) {
-    return client.screenshot()
-        .then((data) => {
-            
-            let buf = new Buffer(data.value, 'base64');
-            let img1 = cv.imdecode(buf)
-            let result = img1.matchTemplate(img, 5).minMaxLoc(); 
-            if (result.maxVal <= 0.65) {
-                // Fail
-                const msg = "Can't see object yet";
-                throw new Error(iCounter === undefined ? msg : msg + " #" + iCounter+"->"+desc);
-            }
-            // All good
-            return result;
-        });
+  return client.screenshot()
+    .then((data) => {
+        
+        let buf = new Buffer(data.value, 'base64');
+        let img1 = cv.imdecode(buf)
+        let result = img1.matchTemplate(img, 5).minMaxLoc(); 
+        if (result.maxVal <= 0.65) {
+            // Fail
+            const msg = "Can't see object yet";
+            throw new Error(iCounter === undefined ? msg : msg + " #" + iCounter+"->"+desc);
+        }
+        // All good
+        return result;
+    });
 }
 
 
 ////// functions used to click on elements -> find based on image -> template matching 
 
 async function fnClick(img,client,repeats=5,desc,wait,offsetX=0,offsetY=0){
-    try{
-      let coordinates= await fnIsOnScreen(img,client,repeats,desc,wait);
-      let xLocation=coordinates.maxLoc.x+(img.cols/2)+offsetX;
-      let yLocation=coordinates.maxLoc.y+(img.rows/2)+offsetY;
-      let performClick= await  client.touchPerform([{
-          action: 'tap',
-          options: {
-              x: xLocation,   // x offset
-              y: yLocation,   // y offset
-              count: 1 // number of touches
-          }
-        }])
-      console.log("Click Performed: "+desc);
-      return(true);
-    }
-    catch(err){
-      console.log(err);
-      throw (err)
-      return(err);
-    }
+  try{
+    let coordinates= await fnIsOnScreen(img,client,repeats,desc,wait);
+    let xLocation=coordinates.maxLoc.x+(img.cols/2)+offsetX;
+    let yLocation=coordinates.maxLoc.y+(img.rows/2)+offsetY;
+    let performClick= await  client.touchPerform([{
+        action: 'tap',
+        options: {
+            x: xLocation,   // x offset
+            y: yLocation,   // y offset
+            count: 1 // number of touches
+        }
+      }])
+    console.log("Click Performed: "+desc);
+    return(true);
+  }
+  catch(err){
+    console.log(err);
+    throw (err)
+  }
     
   
 }
@@ -401,7 +459,7 @@ async function fnClick(img,client,repeats=5,desc,wait,offsetX=0,offsetY=0){
 function fnSaveScreenShot(client){
   console.log("Saving screenshot of passed test");
   return new Promise((resolve,reject)=>{
-      client.screenshot().then((data)=>{
+    client.screenshot().then((data)=>{
           SaveImage(data.value,"passedTest/Screenshot.png")
           .then((data)=>{
              resolve(true)   
@@ -410,7 +468,7 @@ function fnSaveScreenShot(client){
             console.log(err);
           })
          
-      })
+    })
   })
 }
 
@@ -438,26 +496,26 @@ function SaveImage (imageData,imageName){
 async function fnLoginTest(client){
   console.log("Login test running");
   try{
-    let loginClick= await fnClick(imgLoginEmail,client,5,"Email button",500);
-    let confirmAge = await fnClick(imgloginAgeConfirmYes,client,5,"Confirm Age button",2000);
-    let enterEmail = await fnClick(imgloginEnterEmail,client,5,"Enter Email",1200);
+    await fnClick(imgLoginEmail,client,5,"Email button",500);
+    await fnClick(imgloginAgeConfirmYes,client,5,"Confirm Age button",2000);
+    await fnClick(imgloginEnterEmail,client,5,"Enter Email",1200);
     client.keys("matusko.satara@gmail.com");
-    let hideKeyboard = await fnClearKeyBoard(client);
-    let clickNext = await fnClick(imgloginYellowNextButton,client,5,"Next",500);
-    let enterPassword = await fnClick(imgLoginPassword,client,5,"enter Password",2000);
+    await fnClearKeyBoard(client);
+    await fnClick(imgloginYellowNextButton,client,5,"Next",500);
+    await fnClick(imgLoginPassword,client,5,"enter Password",2000);
     client.keys("123123");
-    let hideKeyboard2 = await fnClearKeyBoard(client);
-    let clickNext2 = await fnClick(imgloginYellowNextButton,client,5,"Next",500);
-    let acceptConditions = await fnClick(imgloginAcceptConditions,client,5,"Accept Conditions",500);
-    let clickNext3 = await fnClick(imgloginYellowNextButton,client,5,"Next",500);
-    let clickNext4 = await fnClick(imgloginPassed,client,5,"x",500);
-    let loadingWait = await fnIsOnScreen(imgFriendsForEverX,client,25,"waiting for another loading",2000);
-    let clickNext5 = await fnClick(imgFriendsForEverX,client,5,"x",500);
-    let clickNext6 = await fnClick(imgSettingsButton,client,5,"settings Button",500);
-    let clickNext7 = await fnClick(imgLogOutButton,client,5,"Logout Button",500);
-    let clickNext8 = await fnClick(imgBigOkButton,client,5,"big ok Button",500);
-    let passed = await fnIsOnScreen(imgloginPassLogout,client,20,"if on scree then test passed",2000);
-    let takeScreenShot= await fnSaveScreenShot(client);
+    await fnClearKeyBoard(client);
+    await fnClick(imgloginYellowNextButton,client,5,"Next",500);
+    await fnClick(imgloginAcceptConditions,client,5,"Accept Conditions",500);
+    await fnClick(imgloginYellowNextButton,client,5,"Next",500);
+    await fnClick(imgloginPassed,client,5,"x",500);
+    await fnIsOnScreen(imgFriendsForEverX,client,25,"waiting for another loading (x)",2000);
+    await fnClick(imgFriendsForEverX,client,5,"x",500);
+    await fnClick(imgSettingsButton,client,5,"settings Button",500);
+    await fnClick(imgLogOutButton,client,5,"Logout Button",500);
+    await fnClick(imgBigOkButton,client,5,"big ok Button",500);
+    await fnIsOnScreen(img3,client,20,"if on scree then test passed",2000);
+    await fnSaveScreenShot(client);
     client.end();
   }
   catch(err){
@@ -472,23 +530,23 @@ async function fnLoginTest(client){
 async function fnLoginTest2(client){
   console.log("Login test running");
   try{
-    let loginClick= await fnClick(img3,client,5,"login button",500);
-    let EmailClick= await fnClick(imgLoginEmail,client,5,"Email button",500);
-    let confirmAge = await fnClick(imgloginAgeConfirmYes,client,5,"Confirm Age button",2000);
-    let enterEmail = await fnClick(imgloginEnterEmail,client,5,"Enter Email",1200);
+    await fnClick(img3,client,5,"login button",500);
+    await fnClick(imgLoginEmail,client,5,"Email button",500);
+    await fnClick(imgloginAgeConfirmYes,client,5,"Confirm Age button",2000);
+    await fnClick(imgloginEnterEmail,client,5,"Enter Email",1200);
     client.keys("matusko.satara@gmail.com");
-    let hideKeyboard = await fnClearKeyBoard(client);
-    let clickNext = await fnClick(imgloginYellowNextButton,client,5,"Next",500);
-    let enterPassword = await fnClick(imgLoginPassword,client,5,"enter Password",2000);
+    await fnClearKeyBoard(client);
+    await fnClick(imgloginYellowNextButton,client,5,"Next",500);
+    await fnClick(imgLoginPassword,client,5,"enter Password",2000);
     client.keys("123123");
-    let hideKeyboard2 = await fnClearKeyBoard(client);
-    let clickNext2 = await fnClick(imgloginYellowNextButton,client,5,"Next",500);
-    let clickNext3 = await fnClick(imgFriendsForEverX,client,10,"x",500);
-    let clickNext4 = await fnClick(imgSettingsButton,client,5,"settings Button",500);
-    let clickNext5 = await fnClick(imgLogOutButton,client,5,"Logout Button",500);
-    let clickNext6 = await fnClick(imgBigOkButton,client,5,"big ok Button",500);
-    let passed2 = await fnIsOnScreen(imgloginPassLogout,client,20,"if on scree then test passed",2000);
-    let takeScreenShot= await fnSaveScreenShot(client);
+    await fnClearKeyBoard(client);
+    await fnClick(imgloginYellowNextButton,client,5,"Next",500);
+    await fnClick(imgFriendsForEverX,client,20,"x",500);
+    await fnClick(imgSettingsButton,client,5,"settings Button",500);
+    await fnClick(imgLogOutButton,client,5,"Logout Button",500);
+    await fnClick(imgBigOkButton,client,5,"big ok Button",500);
+    await fnIsOnScreen(imgloginPassed2,client,20,"if on scree then test passed",2000);
+    await fnSaveScreenShot(client);
     client.end();
   }
   catch(err){
