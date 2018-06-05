ae59ce9b3af13db8453afbb6809785a5deff91ea
trix0 <matus.satara@gmail.com>
2018-06-05 23:28:41 +0200

    Buildin it

---
+++ Tests/TestBuilder/Images.js
@@ -0,0 +1,7 @@
+const cv = require('opencv4nodejs');
+
+module.exports.AlertOkButton = cv.imread(__dirname+'/Images/AlertOkButton.png');
+module.exports.AlertOkButton_960 = cv.imread(__dirname+'/Images/AlertOkButton_960.png');
+module.exports.AlertOkButton_1280 = cv.imread(__dirname+'/Images/AlertOkButton_1280.png');
+module.exports.AlertOkButton_1920 = cv.imread(__dirname+'/Images/AlertOkButton_1920.png');
+module.exports.AlertOkButton_2560 = cv.imread(__dirname+'/Images/AlertOkButton_2560.png');
---
+++ Tests/TestBuilder/TestBuilder.js
@@ -0,0 +1,59 @@
+var images = require('./Images.js');
+const express = require('express');
+const app = express();
+const iExpressPort=8764;
+module.exports = function(fn) {
+  return {
+  run:async function GoToProfile(client,testData,testOutput){
+    try{
+        
+
+        let testName=testData.desCaps.testName
+        params=testData.desCaps.parameters;
+        const init=await client.init();    // appium init (lunch app)
+
+        fn.logger.info("Test Builder helper starting");
+
+        //////////////////////////////////// THIS CODE IS MANDATORY -> LEAVE IT HERE
+        let imageSize=await client.windowHandleSize();
+        imageSize=imageSize.value.height;
+        //////////////////////////////////// THIS CODE IS MANDATORY -> LEAVE IT HERE
+        await fnImageRequest(fn,client);
+
+        //fn.fnSaveTestOutput(testOutput,testData.outputDir);
+        //client.end();
+            fn.logger.info("end");
+      }
+
+      catch(err){
+        fn.fnPushToOutputArray({"message":err})
+        fn.fnSaveTestOutput(testOutput,testData.outputDir);
+        client.end();
+        throw err;
+      }
+    }
+  }
+}
+
+function fnImageRequest(fn,client){
+return new Promise(resolve=>{
+    app.listen(iExpressPort, () => console.log('Express started at port '+iExpressPort))
+
+    app.get('/screenshot', async function(req, res) {
+       var screenshot= await client.screenshot();
+        res.send(screenshot)
+    });
+    var interval =setInterval(
+       async function(){ 
+            let activity = await client.currentActivity();
+            fn.logger.info(activity);
+        }
+    , 30000);    
+})
+
+}
+
+
+
+
+
